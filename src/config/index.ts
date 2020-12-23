e256ad0ef5bdcb46e081f9df4bb36b002f2d4d8f
Tem <dvkhuong@vnpt.vn>
2020-12-23 10:39:03 +0700

    config chatbot

--- src/config/index.ts
+++ src/config/index.ts
@@ -82,7 +82,7 @@ export default {
   MAX_MESSAGE_LENGTH: parseEnvNumber('MAX_MESSAGE_LENGTH') || 2000,
 
   // App name (must be the same on Heroku)
-  APP_NAME: parseEnvString('APP_NAME') || 'ptnkchat',
+  APP_NAME: parseEnvString('APP_NAME') || 'hnue-chatbot-finish',
 
   // Page persona profile picture
   PERSONA_PROFILE_PICTURE:
